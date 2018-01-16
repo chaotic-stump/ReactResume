955f330d82f59b4db984a93794948718e1e7780d
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-16 15:03:30 -0500

    add re-renders

--- src/app.js
+++ src/app.js
@@ -21,24 +21,32 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-  console.log('addOne')
+  // count = count + 1;
+  count++;
+  renderCounterApp();
 };
 const minusOne = () => {
-  console.log('minusOne');
+  count--;
+  renderCounterApp();
 };
 const reset = () => {
-  console.log('reset');
+  count = 0;
+  renderCounterApp();
 };
 
-const templateTwo = (
-  <div>
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
     <h1>Count: {count}</h1>
     <button onClick={addOne}>+1</button>
     <button onClick={minusOne}>-1</button>
     <button onClick={reset}>Reset</button>
-  </div>
-);
+    </div>
+  );
+  
+  ReactDOM.render(templateTwo, appRoot);
+};
 
-const appRoot = document.getElementById('app');
-
-ReactDOM.render(templateTwo, appRoot);
+renderCounterApp();
