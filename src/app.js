9b57bd2eadbfa4549b4dfd1d60644d4335646b08
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-16 16:35:25 -0500

    Randomly select option from list of created items

--- src/app.js
+++ src/app.js
@@ -42,6 +42,12 @@ const onRemoveAll = () => {
   render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 
@@ -52,9 +58,8 @@ const render = () => {
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       {app.options.length > 0 ? "Here are your options" : "No options"}
-      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
       <button onClick={onRemoveAll}>Remove all</button>
-
       <ol>
         {
           app.options.map((option) => {
