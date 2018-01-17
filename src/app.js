ba5a0eb89500f06d08e580efbc4e481467549182
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-17 14:40:28 -0500

    creating parent component IndecisionApp

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -19,8 +32,19 @@ class Action extends React.Component {
   }
 }
 
-class Option extends React.Component {
+class Options extends React.Component {
   render() {
+    return (
+      <div>
+        Options - (plural) component here
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render () {
     return (
       <div>
         Option component here
@@ -28,6 +52,7 @@ class Option extends React.Component {
     );
   }
 }
+
 
 class AddOption extends React.Component {
   render() {
@@ -39,15 +64,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    
-    <Header />
-    <Action />
-    <Option />
-    <AddOption />
-
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
