0486c32c0eeb35b2264bd820ba135422c5d0c066
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-17 14:02:41 -0500

    More components exercises - subclasses

--- playground/es6-classes-1.js
+++ playground/es6-classes-1.js
@@ -13,8 +13,50 @@ class Person {
   }
 };
 
-const me = new Person('Sean Parker', 30);
+// sub class through inheritance
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasHomeLocation() {
+    return !!this.homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if(this.hasHomeLocation()) {
+      greeting += ` I'm visiting from ${this.homeLocation}`
+    }
+    
+    return greeting;
+  }
+}
+  
+
+const me = new Traveler('Sean Parker', 30, "Covington");
 console.log(me.getGreeting());
 
-const other = new Person('Seth Jameson', 34);
+const other = new Student('Seth Jameson', 34, 'Communication');
 console.log(other.getDescription());
