b5b53309b4f8940ff10f30d255ef289cb3f96955
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-19 17:53:11 -0500

    Componentizing Visibility toggle

--- playground/build-it-visible.js
+++ playground/build-it-visible.js
@@ -1,27 +1,35 @@
-// Visibility Toggle
-let visible = false;
+// VIsibility Toggle - render, constructor, handleToggleVisibility
+// visibility -> false
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visible: false
+    };
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return{
+        visible: !prevState.visible
 
-let toggleVisibility = () => {
-  visible = !visible;
-  renderVisibleToggle();
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visible ? "Hide Words" : "Show Words"}
+        </button>
+        <p>{this.state.visible && ("WORDS!")}</p>
+
+      </div>
+    );
+  }
 }
 
-// Declare render container
-const appRoot = document.getElementById('app');
 
-const renderVisibleToggle = () => {
-  const visibleTemplate = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visible ? "Hide Words" : "Show Words"}
-      </button>
-      <p>{visible && "WORDS!"}</p>
 
-    </div>
-  );
-    
-  ReactDOM.render(visibleTemplate, appRoot)
-};
-
-renderVisibleToggle();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
