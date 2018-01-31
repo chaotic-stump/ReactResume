79b905154447da90f7cb897e87bd5ea16fd76891
Sean Parker <seanparker@PECOS-MAC06.local>
2018-01-31 14:26:39 -0500

    babel integration into webpack

--- src/app.js
+++ src/app.js
@@ -15,5 +15,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'testing 123');
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
 ReactDOM.render(template, document.getElementById('app'));
