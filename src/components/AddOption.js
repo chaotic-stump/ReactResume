973287a9d773d3dc5a67931d3253bb0edf029749
Sean Parker <seanparker@PECOS-MAC06.local>
2018-02-01 12:01:47 -0500

    remove bindings with transform class properties

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 
 export default class AddOption extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
-  }
-  handleAddOption(e) {
+  state = {
+    error: undefined
+  };
+
+  handleAddOption = (e) => {
     e.preventDefault();
       console.log('testing');
     const option = e.target.elements.option.value.trim();
